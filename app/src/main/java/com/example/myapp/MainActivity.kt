3cd087a8c89874a87d34b56a4f47f6f9b10c5f16
roh-7 <rohit.6233@outlook.com>
2017-07-15 11:18:34 +0530

    browse

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.myapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
+
 
 class MainActivity : AppCompatActivity()
 {
 	val ques = Question()
-	var score:Long = 0
+	var score: Long = 0
+	val GITHUB_URL: String = "https://roh-7.github.io"
 	
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
@@ -17,20 +22,41 @@ class MainActivity : AppCompatActivity()
 		// eliminating use of findViewById using synthetic.
 		trueButton.setOnClickListener { checkAnswer(true) }
 		falseButton.setOnClickListener { checkAnswer(false) }
-		nextButton.setOnClickListener { ques.getNextQuestion()
-		loadQuestion()
+		nextButton.setOnClickListener {
+			ques.getNextQuestion()
+			loadQuestion()
 		}
+	}
+	
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean
+	{
+		menuInflater.inflate(R.menu.options_menu,menu)
+		return true;
+	}
+	
+	override fun onOptionsItemSelected(item: MenuItem?): Boolean
+	{
+		val id = item!!.getItemId()
+		
+		if(id==R.id.share_options)
+		{
+			browse(GITHUB_URL)
+			
+			return true
+		}
+		
+		return super.onOptionsItemSelected(item)
 	}
 	
 	fun loadQuestion()
 	{
-		scoreView.setText("Your score is: "+score)
-		questionTextView.setText("Is "+ques.number+" a prime number ?")
+		scoreView.setText("Your score is: " + score)
+		questionTextView.setText("Is " + ques.number + " a prime number ?")
 	}
 	
-	fun checkAnswer (x: Boolean)
+	fun checkAnswer(x: Boolean)
 	{
-		if(x==ques.answer)
+		if (x == ques.answer)
 		{
 			score++
 			answer_text.setText("Thats correct")
